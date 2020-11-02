c5af2b5878ab06a87c5883df83e83a98ad90b277
MaxPallu <maximilien2776@gmail.com>
2020-11-02 13:19:35 +0100

    Programmation notifications et changements LoginActivity

--- app/src/main/java/com/maxpallu/go4lunch/LoginActivity.java
+++ app/src/main/java/com/maxpallu/go4lunch/LoginActivity.java
@@ -2,23 +2,34 @@ package com.maxpallu.go4lunch;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.OAuthProvider;
 import com.google.firebase.auth.TwitterAuthProvider;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.maxpallu.go4lunch.api.User;
 import com.maxpallu.go4lunch.api.UserHelper;
 import com.maxpallu.go4lunch.base.BaseActivity;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.DefaultLogger;
+import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -34,11 +45,12 @@ public class LoginActivity extends BaseActivity {
     private int RC_SIGN_IN_EMAIL = 789;
     private int RC_SIGN_IN_TWIITER = 246;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth;
     private Button alreadyConnected;
     private Button facebook;
     private Button google;
     private Button email;
-    private Button twitter;
+    private TwitterLoginButton twitter;
 
 
     @Override
@@ -46,20 +58,39 @@ public class LoginActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         alreadyConnected = findViewById(R.id.button_already_connected);
         twitter = findViewById(R.id.twitterLogin);
-        Twitter.initialize(this);
         email = findViewById(R.id.emailSignIn);
+
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(getString(R.string.com_twitter_sdk_android_CONSUMER_KEY), getString(R.string.com_twitter_sdk_android_CONSUMER_SECRET));
+
+        TwitterConfig twitterConfig = new TwitterConfig.Builder(this)
+                .logger(new DefaultLogger(Log.DEBUG))
+                .twitterAuthConfig(authConfig)
+                .build();
+
+        twitter.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                signInWithTwitter(result.data);
+                twitter.setVisibility(View.VISIBLE);
+                getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE, WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+
+            }
+        });
+
+        Twitter.initialize(twitterConfig);
+        Twitter.getInstance();
+        setContentView(R.layout.login_activity);
+
+        mAuth = FirebaseAuth.getInstance();
 
         alreadyConnected.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startMainActivity();
-            }
-        });
-
-        twitter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startTwitterLogin();
             }
         });
 
@@ -69,6 +100,11 @@ public class LoginActivity extends BaseActivity {
                 startEmailLogin();
             }
         });
+    }
+
+    @OnClick(R.id.twitterLogin)
+    public void onClickTwitterLogin() {
+        Toast.makeText(this, "Log In Button Clicked", Toast.LENGTH_SHORT).show();
     }
 
     @OnClick(R.id.login_button)
@@ -126,14 +162,19 @@ public class LoginActivity extends BaseActivity {
                 RC_SIGN_IN_EMAIL);
     }
 
-    private void startTwitterLogin() {
-        TwitterAuthConfig authConfig = new TwitterAuthConfig(getString(R.string.com_twitter_sdk_android_CONSUMER_KEY), getString(R.string.com_twitter_sdk_android_CONSUMER_SECRET));
+    private void signInWithTwitter(TwitterSession session) {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Collections.singletonList(new AuthUI.IdpConfig.TwitterBuilder().build()))
+                        .setIsSmartLockEnabled(false, true)
+                        .build(),
+                RC_SIGN_IN_TWIITER);
 
-        TwitterConfig twitterConfig = new TwitterConfig.Builder(this)
-                .twitterAuthConfig(authConfig)
-                .build();
+        AuthCredential credential = TwitterAuthProvider.getCredential(session.getAuthToken().token,
+                session.getAuthToken().secret);
 
-        Twitter.initialize(twitterConfig);
+        mAuth.signInWithCredential(credential);
     }
 
     private void startGoogleSign() {
--- app/src/main/java/com/maxpallu/go4lunch/notifications/ReminderBroadcast.java
+++ app/src/main/java/com/maxpallu/go4lunch/notifications/ReminderBroadcast.java
@@ -14,6 +14,11 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.maxpallu.go4lunch.R;
 import com.maxpallu.go4lunch.api.UserHelper;
+import com.maxpallu.go4lunch.di.DI;
+import com.maxpallu.go4lunch.models.Workmate;
+import com.maxpallu.go4lunch.service.WorkmateApiService;
+
+import java.util.List;
 
 public class ReminderBroadcast extends BroadcastReceiver {
 
@@ -22,29 +27,43 @@ public class ReminderBroadcast extends BroadcastReceiver {
     private String userId = UserHelper.getUserId();
     private String CHANNEL_ID = "17";
     private int requestId = 100;
+    private WorkmateApiService mApi = DI.getService();
+    private List<Workmate> mWorkmates = mApi.getWorkmates();
+    private Workmate mClients;
 
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        getInformations();
+        getInformations(context);
+    }
+
+    private void getInformations(Context context) {
+        db.collection("user").document(userId).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                restaurant = (String) documentSnapshot.get("restaurantName");
+                for(int i = 0; i<mWorkmates.size(); i++) {
+                    String workmateRestaurant = mWorkmates.get(i).getRestaurantName();
+                    if(restaurant.equals(workmateRestaurant)) {
+                        mClients = mWorkmates.get(i);
+                    }
+                }
+
+                sendNotification(context);
+            }
+        });
+    }
+
+    private void sendNotification(Context context) {
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_image_notification)
                 .setContentTitle("Rappel")
-                .setContentText("Vous mangez à "+restaurant+" avec vos collègues !")
+                .setContentText(context.getString(R.string.notif_choice)+restaurant+ context.getString(R.string.notif_with) +mClients.getName())
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
 
         notificationManager.notify(1, builder.build());
-    }
-
-    private void getInformations() {
-        db.collection("user").document(userId).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-            @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                restaurant = (String) documentSnapshot.get("restaurantName");
-            }
-        });
     }
 }
